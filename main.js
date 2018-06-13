8ceca1735986e3116a7b476ebbd71e259ffada0f
Bunlong Heng <bunlongheng.me@gmail.com>
2018-06-14 01:32:50 +0300

    implemented eventdetails, tooltip get bets for first bookmaker

--- main.js
+++ main.js
@@ -1,11 +1,74 @@
 $(window).on('load', startScraping);
 
 function startScraping() {
-    const events = new Events();
-    console.log('today events', events.todayEvents);
+    if (TodayEvents.isTodayEventsPage()) {
+        const todayEvents = new TodayEvents().todayEvents;
+        window.open(todayEvents[0].link, '_blank');
+    } else { //event details
+        const details = new EventDetails();
+        // alert('cur bet 1: ' + details.currentBet1);
+        // alert('max bet 1: ' + details.maxBet1);
+        window.close()
+    }
 }
 
-class Events {
+class Tooltip {
+    constructor() {
+        this._selector = '#tooltiptext strong:not(:last-of-type)';
+    }
+
+    get coeffs() {
+        return $(this._selector).map((i, v) => +v.innerText).toArray();
+    }
+}
+
+// for first bk
+class EventDetails {
+
+    constructor() {
+        this.betsSelector = '.table-container:first-child tr .odds [onmouseover]';
+    }
+
+    get bets() {
+        return this._bets ? this._bets : this._bets = $(this.betsSelector);
+    }
+
+    getmaxBet(num) {
+        this.bets.get(num).dispatchEvent(new Event('mouseover'));
+        const coeffs = new Tooltip().coeffs;
+        return Math.max(...coeffs);
+    }
+
+    get currentBet1() {
+        return this.getCurrentBet(0);
+    }
+
+    get currentBet2() {
+        return this.getCurrentBet(1);
+    }
+
+    getCurrentBet(num) {
+        return this.bets.eq(num).text();
+    }
+
+    get maxBet1() {
+        return this.getmaxBet(0);
+
+    }
+
+    get maxBet2() {
+        return this.getmaxBet(1);
+    }
+}
+
+class TodayEvents {
+
+    static isTodayEventsPage() {
+        const todayTitleSelector = '#col-content h1';
+        const includesText = 'Today';
+        return $(todayTitleSelector).first().text().includes(includesText);
+    }
+
     constructor() {
         this.eventsSelector = '.table-participant';
     }
@@ -39,20 +102,26 @@ class Events {
         const times = this.todayTimes.toArray();
         const avCoeffs1 = this.averageCoefParticipant1.toArray();
         const avCoeffs2 = this.averageCoefParticipant2.toArray();
-        return {
-            events: this.todayParticipants.toArray().reduce((prev, cur, index) => {
-                prev.push({
-                    participants: cur,
-                    link: links[index],
-                    time: times[index],
-                    average1: avCoeffs1[index],
-                    average2: avCoeffs2[index],
-                    country: '',
-                    tournament: ''
-                });
-                return prev;
-            }, []),
-        }
+        return this.todayParticipants.toArray().reduce((prev, cur, index) => {
+            prev.push({
+                participants: cur,
+                link: links[index],
+                time: times[index],
+                average1: avCoeffs1[index],
+                average2: avCoeffs2[index],
+                country: '',
+                tournament: ''
+            });
+            return prev;
+        }, [])
+
     }
 }
 
+// chrome.runtime.getBackgroundPage(function() {
+//     console.log('should be bg page', this);
+// });
+// chrome.storage.local.set({variable: 'variableInformation'});
+// chrome.storage.local.get(['variable'], function (result) {
+//     console.log('bar', result.variable);
+// });
