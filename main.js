c3fe9bd1c3134590573fdb5c3e8624b7eb843daa
Bunlong Heng <bunlongheng.me@gmail.com>
2018-06-17 03:04:57 +0300

    iteration throw events, set all data to storage extracted function, added js docs

--- main.js
+++ main.js
@@ -1,33 +1,69 @@
-function startScraping() {
-
-    onDetailsCollected(onDetailsCollectedHandler);
-
-    if (TodayEvents.isTodayEventsPage()) {
-        const todayEvents = new TodayEvents().todayEvents;
-        // window.open(todayEvents[0].link, '_blank');
-        window.location.href = todayEvents[0].link;
-    } else { //event details
-        const details = new EventDetails();
-        alert('cur bet 1: ' + details.currentBet1);
-        alert('max bet 1: ' + details.maxBet1);
-        // window.close();
-    }
-}
-
-function onDetailsCollected(callback) {
-    chrome.storage.onChanged.addListener(callback);
-}
-
-function onDetailsCollectedHandler(changes, namespace) {
-    for (key in changes) {
-        var storageChange = changes[key];
-        console.log('Storage key "%s" in namespace "%s" changed. ' +
-            'Old value was "%s", new value is "%s".',
-            key,
-            namespace,
-            storageChange.oldValue,
-            storageChange.newValue);
-    }
-}
-
 $(window).on('load', startScraping);
+
+/**
+ * run script
+ */
+function startScraping() {
+    if (TodayEvents.isTodayEventsPage) {
+        const todayEvents = new TodayEvents().todayEvents, i = 0;
+        setDataToStorage({i, count: todayEvents.length, todayEvents});
+        navigateEventDetailsPage(todayEvents, i);
+    } else { //event details
+        getDataFromStorage(['count', 'i', 'todayEvents'], HandlerEventsDetails)
+    }
+}
+
+/**
+ * event details handler
+ * @param storage
+ * @constructor
+ */
+function HandlerEventsDetails(storage) {
+    let {count, i, todayEvents} = storage;
+    if (++i < count && i < 5) {
+        const details = new EventDetails();
+        todayEvents[i - 1].maxBet1 = details.maxBet1;
+        todayEvents[i - 1].maxBet2 = details.maxBet2;
+        setDataToStorage({i: i + 1, todayEvents});
+        navigateEventDetailsPage(todayEvents, i);
+    } else {
+        onEnd();
+    }
+}
+
+/**
+ * navigate to nextIndex events details page
+ * @param todayEvents
+ * @param nextIndex
+ */
+function navigateEventDetailsPage(todayEvents, nextIndex) {
+    window.location.href = todayEvents[nextIndex].link;
+}
+
+/**
+ * save data to storage
+ * @param dataObj
+ */
+function setDataToStorage(dataObj) {
+    chrome.storage.local.set(dataObj);
+}
+
+/**
+ * get data from storage
+ * @param dataArr properties name array
+ * @param callback function on get
+ */
+function getDataFromStorage(dataArr, callback) {
+    chrome.storage.local.get(dataArr, callback);
+}
+
+/**
+ * on end handler
+ */
+function onEnd() {
+    alert('end');
+    getDataFromStorage(['todayEvents'], (storage) => {
+        console.log(storage.todayEvents);
+    });
+}
+
