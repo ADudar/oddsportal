82a24f12d4f78d616562d30add6231d0436ccf7c
Bunlong Heng <bunlongheng.me@gmail.com>
2018-06-28 00:00:39 +0300

    dropping func improvement

--- filters.js
+++ filters.js
@@ -5,5 +5,17 @@
  * @returns {boolean}
  */
 function droppingBookiesGreaterInPercents(e, droppingPercent) {
-    return (e.openingBet1 - e.currentBet1) / (e.openingBet1 - 1) >= droppingPercent / 100;
+    return isDroppingOdds(e.openingBet1, e.currentBet1, droppingPercent) ||
+        isDroppingOdds(e.openingBet2, e.currentBet2, droppingPercent)
+}
+
+/**
+ * dropping formula
+ * @param openingBet
+ * @param currentBet
+ * @param droppingPercent
+ * @returns {boolean}
+ */
+function isDroppingOdds(openingBet, currentBet, droppingPercent) {
+    return (openingBet - currentBet) / (openingBet - 1) >= droppingPercent / 100;
 }
